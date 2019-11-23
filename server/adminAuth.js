0f8d175073df74513752e9424b0315861a96f7ee
rCarter101 <xorn101@ufl.edu>
2019-11-23 18:03:38 -0500

    Create adminAuth.js

---
+++ server/adminAuth.js
@@ -0,0 +1,47 @@
+const pass = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+var bcrypt = require("bcryptjs");//crypto library
+
+app.use(pass.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done){
+	done(null, user._id);
+});
+
+passport.deserializeUser(function(userId, done){
+	User.findById(userId, (err, user) => done(err, user));
+});
+
+const local = new LocalStrategy((username, password, done) => {//password/username verification
+	User.findOne({ username })
+		.then(user =>{
+			if(!user) {
+				done(null, false, {message: "Oops! That username does not exist!"});
+			} 
+			else{
+				bcrypt.compare(password, user.password, function(err, response){
+					if(err) return done(err);
+					if(res==false){
+						return done(null, false);
+					}
+					else{
+						return done(null, user);
+					}
+				});
+			}
+		});
+	.catch(e => done(e));
+});
+
+passport.use("local", local);
+
+app.post('/register', function(req, res, next){
+	bcrypt.gensalt(10, function(err, salt){
+		if(err) return next(err);
+		bcrypt.hash(req.body.password, salt, function(err, hash){
+			if(err) return next(err);
+			newUser.password = hash;
+		});
+	});
+});
