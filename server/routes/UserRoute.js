db0285caccaefce5e7a5708c5b05aadba82512f6
Boris Ermakov-Spektor <b.ermakovspektor@ufl.edu>
2019-11-28 09:31:04 -0500

    Finish authentication in backend

---
+++ server/routes/UserRoute.js
@@ -0,0 +1,13 @@
+const userController = require('../controllers/UserController');
+const express = require('express');
+const router = new express.Router();
+
+router.route('/login').post(userController.login);
+
+router.route('/logout').post(userController.logout);
+
+router.route('/isLoggedIn').post(userController.isLoggedIn);
+
+router.route('/register').post(userController.register);
+
+module.exports = router;
